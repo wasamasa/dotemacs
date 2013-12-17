f3c31aae468fc494bc60c09ae9e6564d06b9f169
Vasilij Schneidermann <v.schneidermann@gmail.com>
2013-12-17 14:26:14 +0100

    Added helm narrowing of autocomplete candidates

--- personal-elisp/wasa-autocomplete.el
+++ personal-elisp/wasa-autocomplete.el
@@ -3,5 +3,6 @@
       ac-quick-help-delay 0.3
       ac-comphist-file (concat user-emacs-directory "etc/ac-comphist"))
 (ac-company-define-source ac-source-company-elisp company-elisp)
+(define-key ac-complete-mode-map (kbd "C-:") 'ac-complete-with-helm)
 
 (provide 'wasa-autocomplete)
