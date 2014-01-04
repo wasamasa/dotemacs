87143c4561a8f0eca1c5f43787e9455b4c331814
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-01-04 08:43:00 +0100

    Added generic string function for needle-haystack-test, cleaned up existing functions to use cl-lib

--- personal-elisp/wasa-defuns.el
+++ personal-elisp/wasa-defuns.el
@@ -1,11 +1,15 @@
+;; -*- lexical-binding: t -*-
+(require 'cl-lib)
+(require 's)
+
 (defun wasa-replace-in-region-or-buffer (search-replace-list)
   (let ((beg (if (region-active-p) (region-beginning) (point-min)))
         (end (if (region-active-p) (region-end) (point-max)))
         (case-fold-search nil))
     (save-excursion
-      (dolist (sr search-replace-list)
+      (cl-dolist (sr search-replace-list)
         (goto-char beg)
-        (while (search-forward (car sr) end t)
+        (while (re-search-forward (car sr) end t)
           (replace-match (cadr sr) t t))))))
 
 (defun wasa-replace-paragraphs ()
@@ -28,5 +32,11 @@
   (unless (region-active-p)
     (setq beg 0)(setq end 0))
   (message "Region: %d %d" beg end))
+
+(defun wasa-any-regex-in-string (regexes string)
+  "Tests whether any of the given regular expressions is present in the given string."
+  (cl-dolist (regex regexes)
+    (when (s-contains? regex string)
+      (cl-return t))))
 
 (provide 'wasa-defuns)
