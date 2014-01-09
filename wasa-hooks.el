2750d9a6e77ca49e9f0af31ac47aac0091fb60ed
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-01-09 13:20:30 +0100

    Remove useless function call

---
+++ wasa-hooks.el
@@ -0,0 +1,16 @@
+(defun wasa-turn-on-fill-mode ()
+  (auto-fill-mode 1))
+(defun wasa-turn-on-indicate-empty-lines ()
+  (setq indicate-empty-lines t))
+(defun wasa-turn-on-indicate-buffer-boundaries ()
+  (setq indicate-buffer-boundaries
+        '((top . left) (bottom . left))))
+
+(add-hook 'text-mode-hook 'wasa-turn-on-indicate-buffer-boundaries)
+(add-hook 'text-mode-hook 'wasa-turn-on-indicate-empty-lines)
+(add-hook 'text-mode-hook 'wasa-turn-on-fill-mode)
+(add-hook 'prog-mode-hook 'wasa-turn-on-indicate-buffer-boundaries)
+(add-hook 'prog-mode-hook 'wasa-turn-on-indicate-empty-lines)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode-enable)
+
+(provide 'wasa-hooks)
