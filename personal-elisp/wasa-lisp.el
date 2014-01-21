13dcd33c4c4a674d91a37c6a4efdd03b2b67319d
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-01-21 22:08:44 +0100

    Add extra highlighting for cl-lib functions

--- personal-elisp/wasa-lisp.el
+++ personal-elisp/wasa-lisp.el
@@ -2,6 +2,8 @@
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             (add-to-list 'ac-sources 'ac-source-company-elisp)))
+(cl-lib-highlight-initialize)
+(cl-lib-highlight-warn-cl-initialize)
 (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (setq nrepl-hide-special-buffers t)
 (setq show-paren-delay 0.05)
