5f280deb5f921db20d98a4fbcbc39fc6f7b71d11
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-02-04 08:42:48 +0100

    Set company backends and settings

--- personal-elisp/wasa-company.el
+++ personal-elisp/wasa-company.el
@@ -1,4 +1,13 @@
-(setq company-idle-delay t)
+(setq company-idle-delay 0.1
+      company-minimum-prefix-length 2
+      company-selection-wrap-around t
+      company-show-numbers t)
+(setq company-backends
+      '(company-elisp company-nxml company-css company-capf
+                      (company-gtags company-etags
+                                     company-dabbrev-code company-keywords)
+                      company-files company-dabbrev))
+(add-hook 'prog-mode-hook 'company-mode-on)
 (define-key company-active-map (kbd "C-:") 'helm-company)
 
 (provide 'wasa-company)
