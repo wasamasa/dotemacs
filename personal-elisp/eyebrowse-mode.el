b82aefd3ea973f87e081beabbd398da09fbef791
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-02-17 23:54:10 +0100

    Add skeleton

---
+++ personal-elisp/eyebrowse-mode.el
@@ -0,0 +1,21 @@
+(defvar eyebrowse-lighter "¬_¬")
+(defvar eyebrow-mode-line-position 'right)
+(defvar eyebrowse-last-window-config)
+(defvar eyebrowse-current-window-config)
+(defvar eyebrowse-window-configs)
+
+(defun eyebrowse-save-window-config (slot))
+
+(defun eyebrowse-load-window-config (slot))
+
+(defun eyebrowse-switch-to-window-config (slot))
+
+(defun eyebrowse-next-window-config ())
+
+(defun eyebrowse-prev-window-config ())
+
+(defun eyebrowse-last-window-config ())
+
+(defun eyebrowse-delete-window-config (slot))
+
+(defun eyebrowse-update-mode-line ())
