12ebf1ab6c6787002df88c8390ea1ba7e304a0f3
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-02-18 19:22:08 +0100

    Add docstrings

--- personal-elisp/eyebrowse-mode.el
+++ personal-elisp/eyebrowse-mode.el
@@ -1,21 +1,86 @@
-(defvar eyebrowse-lighter "¬_¬")
-(defvar eyebrow-mode-line-position 'right)
-(defvar eyebrowse-last-window-config)
-(defvar eyebrowse-current-window-config)
-(defvar eyebrowse-window-configs)
+(defcustom eyebrowse-lighter "¬_¬"
+  "Lighter for the `eyebrowse-minor-mode'")
 
-(defun eyebrowse-save-window-config (slot))
+(defcustom eyebrowse-mode-line-position 'right
+  "Position of the mode line indicator.
+It can be either 'left or 'right")
 
-(defun eyebrowse-load-window-config (slot))
+(defcustom eyebrowse-display-mode-line-when-only-one-slot-p t
+  "Show mode line indicator when there's only one window
+configuration?
+If t, hide.
+If nil, show.")
 
-(defun eyebrowse-switch-to-window-config (slot))
+(defcustom eyebrowse-restore-point-p t
+  "Restore point, too?
+If t, restore point.")
 
-(defun eyebrowse-next-window-config ())
+(defcustom eyebrowse-frame-local-p nil
+  "Should eyebrowse use an indicator per frame?
+If t, use an indicator per frame.
+If nil, use one indicator for all frames.")
 
-(defun eyebrowse-prev-window-config ())
+(defcustom eyebrowse-wrap-around-p nil
+  "Wrap around when switching to the next/previous window config?
+If t, wrap around.")
 
-(defun eyebrowse-last-window-config ())
+(defcustom eyebrowse-switch-back-and-forth-p nil
+  "When switching to the same window config as
+`eyebrowse-current-window-config', do switch to
+`eyebrowse-last-window-config' or not?
+If t, switch back and forth.")
 
-(defun eyebrowse-delete-window-config (slot))
+;; (defcustom eyebrowse-undo-p nil
+;;   "Use a simplistic undo system?
+;; If t, the last window config will be restored in its killed
+;; slot.")
 
-(defun eyebrowse-update-mode-line ())
+(defvar eyebrowse-last-slot nil
+  "Internal variable storing the last window config slot.")
+
+(defvar eyebrowse-current-slot nil
+  "Internal variable storing the current window config slot.")
+
+(defvar eyebrowse-window-configs nil
+  "Internal variable storing all window configs.")
+
+(defun eyebrowse-save-window-config (slot)
+  "Save the current window config to SLOT.")
+
+(defun eyebrowse-load-window-config (slot)
+  "Restore the window config from SLOT.")
+
+(defun eyebrowse-switch-to-window-config (slot)
+  "Switch to the window config SLOT.  This will save the current
+window config to `eyebrowse-current-slot' first, then switch. If
+`eyebrowse-switch-back-and-forth-p' is t, this will switch to the
+last window config.")
+
+(defun eyebrowse-next-window-config ()
+  (interactive)
+  "Switch to the next available window config.  If
+`eyebrowse-wrap-around-p' is t, this will switch from the last to
+the first one.")
+
+(defun eyebrowse-prev-window-config ()
+  (interactive)
+  "Switch to the previous available window config.  If
+`eyebrowse-wrap-around-p' is t, this will switch from the first
+to the last one.")
+
+(defun eyebrowse-last-window-config ()
+  (interactive)
+  "Switch to the last window config.")
+
+(defun eyebrowse-delete-window-config (slot)
+  "Remove the window config at SLOT.")
+
+(defun eyebrowse-close-window-config ()
+  (interactive)
+  "Close the current window config and remove it from
+`eyebrowse-window-configs'")
+
+(defun eyebrowse-update-mode-line ()
+  "Redraw the relevant part of the mode line.")
+
+;; bind M-1..9 to (lambda () (interactive) (eyebrowse-switch-to-window-config 1..9))
