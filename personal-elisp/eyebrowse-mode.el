4d7b6bdbaeb864332a52bf974a78fbb189947e31
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-02-22 17:35:37 +0100

    Implement and improve some functions

--- personal-elisp/eyebrowse-mode.el
+++ personal-elisp/eyebrowse-mode.el
@@ -74,8 +74,9 @@ NEW-ELEMENT"
 
 (defun eyebrowse-delete-window-config (slot)
   "Remove the window config at SLOT."
-  ;; TODO
-)
+  (setq eyebrowse-window-configs
+        (remove (assq slot eyebrowse-window-configs)
+                eyebrowse-window-configs)))
 
 (defun eyebrowse-update-mode-line ()
   "Redraw the relevant part of the mode line."
@@ -92,8 +93,9 @@ window config to `eyebrowse-current-slot' first, then switch. If
 last window config."
   (interactive)
   ;; FIXME make (interactive) do something
-  (when (or (and eyebrowse-switch-back-and-forth-p (= eyebrowse-current-slot slot))
-          (/= eyebrowse-current-slot slot))
+  (when (or (and eyebrowse-switch-back-and-forth-p
+                 (= eyebrowse-current-slot slot))
+            (/= eyebrowse-current-slot slot))
     (eyebrowse-save-window-config eyebrowse-current-slot)
     (eyebrowse-load-window-config slot)
     (setq eyebrowse-last-slot eyebrowse-current-slot)
@@ -104,16 +106,28 @@ last window config."
 `eyebrowse-wrap-around-p' is t, this will switch from the last to
 the first one."
   (interactive)
-  ;; TODO
-)
+  (let* ((match (assq eyebrowse-current-slot eyebrowse-window-configs))
+         (index (-elem-index match eyebrowse-window-configs))
+         (next-index (car (nth (1+ index) eyebrowse-window-configs))))
+    (if (< (1+ index) (length eyebrowse-window-configs))
+        (eyebrowse-switch-to-window-config next-index)
+      (when eyebrowse-wrap-around-p
+        (eyebrowse-switch-to-window-config
+         (car (car eyebrowse-window-configs)))))))
 
 (defun eyebrowse-prev-window-config ()
   "Switch to the previous available window config.  If
 `eyebrowse-wrap-around-p' is t, this will switch from the first
 to the last one."
   (interactive)
-  ;; TODO
-)
+  (let* ((match (assq eyebrowse-current-slot eyebrowse-window-configs))
+         (index (-elem-index match eyebrowse-window-configs))
+         (prev-index (car (nth (1- index) eyebrowse-window-configs))))
+    (if (> index 0)
+        (eyebrowse-switch-to-window-config prev-index)
+      (when eyebrowse-wrap-around-p
+        (eyebrowse-switch-to-window-config
+         (car (car (last eyebrowse-window-configs))))))))
 
 (defun eyebrowse-last-window-config ()
   "Switch to the last window config."
@@ -121,11 +135,16 @@ to the last one."
   (eyebrowse-switch-to-window-config eyebrowse-last-slot))
 
 (defun eyebrowse-close-window-config ()
-  "Close the current window config and remove it from
-`eyebrowse-window-configs'"
+  "Close the current window config, remove it from
+`eyebrowse-window-configs' and switch to another appropriate
+window config."
   (interactive)
-  ;; TODO
-)
+  (when (> (length eyebrowse-window-configs) 1)
+    (if (equal (assq eyebrowse-current-slot eyebrowse-window-configs)
+               (car (last eyebrowse-window-configs)))
+        (eyebrowse-prev-window-config)
+      (eyebrowse-next-window-config))
+    (eyebrowse-delete-window-config eyebrowse-last-slot)))
 
 (defun eyebrowse-setup-keys (variant)
   "Set up the key bindings for using eyebrowse. VARIANT is one of
