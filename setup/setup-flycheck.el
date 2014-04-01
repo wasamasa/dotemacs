d18cb6d47d826f958787942920bc8d2cb8511454
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-04-01 16:26:45 +0200

    Make flycheck only work on save

--- setup/setup-flycheck.el
+++ setup/setup-flycheck.el
@@ -1,3 +1,4 @@
-(setq flycheck-disabled-checkers '(tex-chktex))
+(setq flycheck-disabled-checkers '(tex-chktex)
+      flycheck-check-syntax-automatically '(mode-enabled save))
 
 (provide 'setup-flycheck)
