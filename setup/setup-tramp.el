2a5273281c1c983a82fe0379b2157d624cbf4443
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-04-02 20:39:21 +0200

    Use eval-after

--- setup/setup-tramp.el
+++ setup/setup-tramp.el
@@ -1,10 +1,11 @@
-(setq tramp-persistency-file-name (concat user-emacs-directory "etc/tramp"))
-(eval-after-load "tramp-sh"
-  '(setq tramp-remote-process-environment
-         (cons "HISTFILE=''"
-               (remove "HISTFILE=$HOME/.tramp_history"
-                       tramp-remote-process-environment))))
-(defadvice tramp-maybe-open-connection (before no-hist activate)
-  (setenv "HISTFILE" ""))
+(my-eval-after tramp-cache
+  (setq tramp-persistency-file-name (concat user-emacs-directory "etc/tramp")))
+(my-eval-after tramp-sh
+  (setq tramp-remote-process-environment
+        (cons "HISTFILE=''"
+              (remove "HISTFILE=$HOME/.tramp_history"
+                      tramp-remote-process-environment)))
+  (defadvice tramp-maybe-open-connection (before no-hist activate)
+    (setenv "HISTFILE" "")))
 
 (provide 'setup-tramp)
