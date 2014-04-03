886a337767a6978cc42b8493d2b8ddcacba40b36
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-04-03 20:01:27 +0200

    Eval after load

--- setup/setup-auctex.el
+++ setup/setup-auctex.el
@@ -1,52 +1,102 @@
-(setq TeX-engine 'luatex)
-(setq TeX-quote-after-quote t
-      TeX-auto-save t
-      TeX-parse-self t
-      TeX-view-program-list '(("llpp" "llpp %o"))
-      TeX-view-program-selection '(((output-dvi style-pstricks)
-                                    "dvips and gv")
-                                   (output-dvi "xdvi")
-                                   (output-pdf "llpp")
-                                   (output-html "xdg-open")))
+(defun my-turn-on-pretty-symbols-mode ()
+  (pretty-symbols-mode 1)
+  (font-latex-setup)
+  (font-lock-fontify-buffer))
+
 (defun my-enable-auctex-pdf-mode ()
   (TeX-global-PDF-mode 1))
-(add-hook 'LaTeX-mode-hook 'my-enable-auctex-pdf-mode)
-(add-hook 'LaTeX-mode-hook 'latex/setup-keybinds)
+
 (autoload 'latex/section-regexp "latex-extra" "LaTeX section regexp" t)
 (defun my-extend-hs-modes-alist ()
   (add-to-list 'hs-special-modes-alist
                `(latex-mode ,(latex/section-regexp) nil "%"
                             (lambda (arg) (latex/next-section 1)
                               (skip-chars-backward " \t\n")) nil)))
-(add-hook 'LaTeX-mode-hook 'my-extend-hs-modes-alist)
 
-(defun my-turn-on-pretty-symbols-mode ()
-  (pretty-symbols-mode 1)
-  (font-latex-setup)
-  (font-lock-fontify-buffer))
-(setq TeX-install-font-lock 'my-turn-on-pretty-symbols-mode)
-(add-hook 'LaTeX-mode-hook 'my-turn-on-pretty-symbols-mode)
+(my-eval-after tex
+  (setq TeX-engine 'luatex
+        TeX-quote-after-quote t
+        TeX-auto-save t
+        TeX-parse-self t
+        TeX-view-program-list '(("llpp" "llpp %o"))
+        TeX-view-program-selection '(((output-dvi style-pstricks)
+                                      "dvips and gv")
+                                     (output-dvi "xdvi")
+                                     (output-pdf "llpp")
+                                     (output-html "xdg-open"))
+        TeX-install-font-lock 'my-turn-on-pretty-symbols-mode)
 
-(setq LaTeX-section-hook '(my-LaTeX-section-heading
-                           LaTeX-section-title
-                           LaTeX-section-section))
+  (defun TeX-command-master (&optional override-confirm)
+    "Run command on the current document.
 
-(defun my-LaTeX-section-heading ()
-  "Hook to prompt for LaTeX section name.
+If a prefix argument OVERRIDE-CONFIRM is given, confirmation will
+depend on it being positive instead of the entry in `TeX-command-list'."
+    (interactive "P")
+    (TeX-command (my-TeX-command-query (TeX-master-file)) 'TeX-master-file
+                 override-confirm))
+
+
+  (defun TeX-command-query (name)
+    "Query the user for what TeX command to use."
+    (let* ((default
+             (cond ((if (string-equal name TeX-region)
+                        (TeX-check-files (concat name "." (TeX-output-extension))
+                                         (list name)
+                                         TeX-file-extensions)
+                      (TeX-save-document (TeX-master-file)))
+                    TeX-command-default)
+                   ((and (memq major-mode '(doctex-mode latex-mode))
+                         ;; Want to know if bib file is newer than .bbl
+                         ;; We don't care whether the bib files are open in emacs
+                         (TeX-check-files (concat name ".bbl")
+                                          (mapcar 'car
+                                                  (LaTeX-bibliography-list))
+                                          (append BibTeX-file-extensions
+                                                  TeX-Biber-file-extensions)))
+                    ;; We should check for bst files here as well.
+                    (if LaTeX-using-Biber TeX-command-Biber TeX-command-BibTeX))
+                   ((TeX-process-get-variable name
+                                              'TeX-command-next
+                                              TeX-command-Show))
+                   (TeX-command-Show)))
+           (completion-ignore-case t)
+           (answer (or TeX-command-force
+                       (completing-read
+                        (concat "Command: (default " default ") ")
+                        (TeX-mode-specific-command-list major-mode) nil t
+                        default 'TeX-command-history))))
+      ;; If the answer is "latex" it will not be expanded to "LaTeX"
+      (setq answer (car-safe (TeX-assoc answer TeX-command-list)))
+      (if (and answer
+               (not (string-equal answer "")))
+          answer
+        default))))
+
+(my-eval-after latex
+  (add-hook 'LaTeX-mode-hook 'my-enable-auctex-pdf-mode)
+  (add-hook 'LaTeX-mode-hook 'latex/setup-keybinds)
+  (add-hook 'LaTeX-mode-hook 'my-extend-hs-modes-alist)
+  (add-hook 'LaTeX-mode-hook 'my-turn-on-pretty-symbols-mode)
+  (setq LaTeX-section-hook '(my-LaTeX-section-heading
+                             LaTeX-section-title
+                             LaTeX-section-section))
+
+  (defun LaTeX-section-heading ()
+    "Hook to prompt for LaTeX section name.
 Insert this hook into `LaTeX-section-hook' to allow the user to change
 the name of the sectioning command inserted with `\\[LaTeX-section]'."
-  (let ((string (completing-read
-		 (concat "Level: (default " name ") ")
-		 LaTeX-section-list
-		 nil nil name)))
-    ; Update name
-    (if (not (zerop (length string)))
-	(setq name string))
-    ; Update level
-    (setq level (LaTeX-section-level name))))
+    (let ((string (completing-read
+                   (concat "Level: (default " name ") ")
+                   LaTeX-section-list
+                   nil nil name)))
+      ; Update name
+      (if (not (zerop (length string)))
+          (setq name string))
+      ; Update level
+      (setq level (LaTeX-section-level name))))
 
-(defun my-LaTeX-environment (arg)
-  "Make LaTeX environment (\\begin{...}-\\end{...} pair).
+  (defun LaTeX-environment (arg)
+    "Make LaTeX environment (\\begin{...}-\\end{...} pair).
 With optional ARG, modify current environment.
 
 It may be customized with the following variables:
@@ -61,80 +111,29 @@ It may be customized with the following variables:
 `LaTeX-default-width'             Width for minipage and tabular*.
 `LaTeX-default-position'          Position for array and tabular."
 
-  (interactive "*P")
-  (let ((environment (completing-read (concat "Environment type: (default "
-					       (if (TeX-near-bobp)
-						   "document"
-						 LaTeX-default-environment)
-					       ") ")
-				      (LaTeX-environment-list) nil t nil
-				      'LaTeX-environment-history LaTeX-default-environment)))
-    ;; Get default
-    (cond ((and (zerop (length environment))
-		(TeX-near-bobp))
-	   (setq environment "document"))
-	  ((zerop (length environment))
-	   (setq environment LaTeX-default-environment))
-	  (t
-	   (setq LaTeX-default-environment environment)))
+    (interactive "*P")
+    (let ((environment (completing-read (concat "Environment type: (default "
+                                                (if (TeX-near-bobp)
+                                                    "document"
+                                                  LaTeX-default-environment)
+                                                ") ")
+                                        (LaTeX-environment-list) nil t nil
+                                        'LaTeX-environment-history LaTeX-default-environment)))
+      ;; Get default
+      (cond ((and (zerop (length environment))
+                  (TeX-near-bobp))
+             (setq environment "document"))
+            ((zerop (length environment))
+             (setq environment LaTeX-default-environment))
+            (t
+             (setq LaTeX-default-environment environment)))
 
-    (let ((entry (assoc environment (LaTeX-environment-list))))
-      (if (null entry)
-	  (LaTeX-add-environments (list environment)))
+      (let ((entry (assoc environment (LaTeX-environment-list))))
+        (if (null entry)
+            (LaTeX-add-environments (list environment)))
 
-      (if arg
-	  (LaTeX-modify-environment environment)
-	(LaTeX-environment-menu environment)))))
-
-(defun my-TeX-command-master (&optional override-confirm)
-  "Run command on the current document.
-
-If a prefix argument OVERRIDE-CONFIRM is given, confirmation will
-depend on it being positive instead of the entry in `TeX-command-list'."
-  (interactive "P")
-  (TeX-command (my-TeX-command-query (TeX-master-file)) 'TeX-master-file
-	       override-confirm))
-
-
-(defun my-TeX-command-query (name)
-  "Query the user for what TeX command to use."
-  (let* ((default
-	   (cond ((if (string-equal name TeX-region)
-		      (TeX-check-files (concat name "." (TeX-output-extension))
-				       (list name)
-				       TeX-file-extensions)
-		    (TeX-save-document (TeX-master-file)))
-		  TeX-command-default)
-		 ((and (memq major-mode '(doctex-mode latex-mode))
-		       ;; Want to know if bib file is newer than .bbl
-		       ;; We don't care whether the bib files are open in emacs
-		       (TeX-check-files (concat name ".bbl")
-					(mapcar 'car
-						(LaTeX-bibliography-list))
-					(append BibTeX-file-extensions
-						TeX-Biber-file-extensions)))
-		  ;; We should check for bst files here as well.
-		  (if LaTeX-using-Biber TeX-command-Biber TeX-command-BibTeX))
-		 ((TeX-process-get-variable name
-					    'TeX-command-next
-					    TeX-command-Show))
-		 (TeX-command-Show)))
-         (completion-ignore-case t)
-         (answer (or TeX-command-force
-                     (completing-read
-                      (concat "Command: (default " default ") ")
-                      (TeX-mode-specific-command-list major-mode) nil t
-                      default 'TeX-command-history))))
-    ;; If the answer is "latex" it will not be expanded to "LaTeX"
-    (setq answer (car-safe (TeX-assoc answer TeX-command-list)))
-    (if (and answer
-             (not (string-equal answer "")))
-        answer
-      default)))
-
-(defun my-LaTeX-hook ()
-  (local-set-key (kbd "C-c C-e") 'my-LaTeX-environment)
-  (local-set-key (kbd "C-c C-c") 'my-TeX-command-master))
-(add-hook 'LaTeX-mode-hook 'my-LaTeX-hook)
+        (if arg
+            (LaTeX-modify-environment environment)
+          (LaTeX-environment-menu environment))))))
 
 (provide 'setup-auctex)
