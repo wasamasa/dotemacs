7ad91d2380f80e77350f30066c9a046b22643fb6
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-04-04 11:08:16 +0200

    Use eval-after

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -1,4 +1,15 @@
 (my-eval-after company
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 2
+        company-selection-wrap-around t
+        company-show-numbers t
+        company-global-modes '(nxml-mode sgml-mode css-mode
+                                         inferior-emacs-lisp-mode)
+        company-backends '(company-jedi company-elisp company-nxml company-css
+                                        company-capf (company-gtags company-etags
+                                        company-dabbrev-code company-keywords)
+                                        company-files company-dabbrev)
+        company-jedi-python-bin "python")
   (defun company-mode-on ()
     (when (and (not (or noninteractive (eq (aref (buffer-name) 0) ?\s)))
                (cond ((eq company-global-modes t)
@@ -7,21 +18,9 @@
                       (not (memq major-mode (cdr company-global-modes))))
                      (t (or (not (null (derived-mode-p 'prog-mode)))
                             (memq major-mode company-global-modes)))))
-      (company-mode 1))))
+      (company-mode 1)))
+  (define-key company-active-map (kbd "C-:") 'helm-company))
 
 (global-company-mode t)
-(define-key company-active-map (kbd "C-:") 'helm-company)
-
-(setq company-idle-delay 0.1
-      company-minimum-prefix-length 2
-      company-selection-wrap-around t
-      company-show-numbers t
-      company-global-modes '(nxml-mode sgml-mode css-mode
-                             inferior-emacs-lisp-mode)
-      company-backends '(company-jedi company-elisp company-nxml company-css
-                         company-capf (company-gtags company-etags
-                                       company-dabbrev-code company-keywords)
-                         company-files company-dabbrev))
-(setq company-jedi-python-bin "python")
 
 (provide 'setup-company)
