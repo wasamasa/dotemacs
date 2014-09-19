4a8502b83c31a12885d33cd4b56ddc78ea39e44a
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-09-19 07:57:59 +0200

    Add TODOs, rename

--- unpublished/blip.el
+++ unpublished/blip.el
@@ -1,34 +1,76 @@
-(defvar boop-player-process nil)
+;; lexical-binding: t
 
-(defun boop-create-player-process ()
+;; Sound basics
+;; play/pacat/aplay/... allow you to send raw audio
+;; for the 8bit sound we'll use a sample rate of 22,050 Hz and u8 as bit depth
+;; the former means 22,050 values make up a second of sound
+;; the latter that the values are between 0 and 255
+;; if you'd plot the resulting graph, you'd see wave forms
+;; so, if you send the right ascii characters, you'll get sawtooths and such
+;; timbre is affected by the general wave form
+;; loudness by the amplitude of the wave form
+;; multiple sounds need to be mixed to a final wave form
+;; modification of existing wave forms is done by filtering
+
+(defvar blip-player-process nil)
+(defvar blip-audio-buffer (get-buffer-create "*blip audio buffer*"))
+(defvar blip-samplerate 22050)
+(defvar blip-bitdepth 256)
+
+(defun blip-create-player-process ()
   (let ((process-connection-type nil))
-    ;; TODO test whether pacat is available, if not use aplay
     ;; TODO make it customizable
-    ;; TODO set correct sample rate (should be 8000?)
-    (setq boop-player-process (start-process "pacat" "*pacat*" "pacat"))))
+    (if (executable-find "pacat")
+        (setq blip-player-process
+              (start-process "pacat" "*pacat*" "pacat" "--format=u8" "--rate=22050"))
+      (setq blip-player-process
+            (start-process "aplay" "*aplay*" "aplay" "--format=U8" "--rate=22050")))))
 
-(defun boop-send-int-to-player (int)
-  ;; TODO make sure mod 65535 is the right choice
-  (process-send-string boop-player-process (string (mod int 65535))))
+(defun blip-ensure-process ()
+  (unless (and blip-player-process (process-live-p blip-player-process))
+    (blip-create-player-process)))
 
-;; TODO turn this into a closure, then generator
-(defun boop-send-ints-to-player (computation)
+(defun blip-send-int-to-player (int)
+  (process-send-string blip-player-process (string int)))
+
+;; TODO make it controllable (closures?)
+;; (boop (lambda (i) (mod i 256)))
+;; => plays sawtooth
+;; (boop (lambda (i) (mod (* 2 i) 256)))
+;; => makes it play a higher-pitched sawtooth
+;; (boop)
+;; stops playback
+
+;; ignore this below
+
+(let ((thunk (lambda (n) (1+ n))))
+  (defun blip (new-thunk)
+    (setq thunk new-thunk))
+
+  (defun create-blip ()
+    (let ((counter 0))
+      (lambda ()
+        (setq counter (1+ counter))
+        (funcall thunk counter)))))
+
+;; and that above, too
+
+;; TODO this shouldn't block emacs
+;; TODO after that's solved, control playback
+(defun blip-send-ints-to-player (thunk)
   (let ((i 0))
     (while t
-      (boop-send-int-to-player (funcall computation i))
+      (blip-send-int-to-player (funcall thunk i))
       (setq i (1+ i)))))
 
-;; TODO use something simpler
-;; TODO make a c->elisp macro?
-(defun boop-player-int-function (i)
-  ;; infix notation: (t>>9&t>>8)*(t>>3)
-  (* (logand (lsh i -9) (lsh i -8)) (lsh i -3)))
+(defun blip-player-int-function (i)
+  ;; infix: (t>>9&t>>8)*(t>>3)
+  (* (logand (lsh i -9) (lsh i -8)) (lsh -i 3)))
 
-;; TODO add functions for stopping, resetting, replacing
-(defun boop-play-some-music ()
+(defun blip-play-loop ()
   (interactive)
-  (unless (and boop-player-process (process-live-p boop-player-process))
-    (boop-create-player-process))
-  (boop-send-ints-to-player 'boop-player-int-function))
+  (blip-ensure-process)
+  (blip-send-ints-to-player 'blip-player-int-function))
 
 ;; TODO do a function that renders to file, ideally with loop detection
+;; TODO graph resulting waveform to ascii
