67610097571b3fbd342d895700194f6d55164a03
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-09-19 08:02:45 +0200

    Make backtraces show line numbers

--- setup/setup-annoyances.el
+++ setup/setup-annoyances.el
@@ -63,6 +63,73 @@
       ediff-split-window-function 'split-window-horizontally)
 (setq quelpa-upgrade-p t)
 
+;; great candidate for a patch that enables a more convenient option
+(my-eval-after debug
+  (defun debugger-setup-buffer (debugger-args)
+    "Initialize the `*Backtrace*' buffer for entry to the debugger.
+That buffer should be current already."
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (set-buffer-multibyte t)		;Why was it nil ?  -stef
+    (setq buffer-undo-list t)
+    (let ((standard-output (current-buffer))
+          (print-escape-newlines t)
+          (print-level 8)
+          (print-length 50))
+      (backtrace))
+    (goto-char (point-min))
+    (delete-region (point)
+                   (progn
+                     (search-forward "\n  debug(")
+                     (forward-line (if (eq (car debugger-args) 'debug)
+                                       2	; Remove implement-debug-on-entry frame.
+                                     1))
+                     (point)))
+    (insert "Debugger entered")
+    ;; lambda is for debug-on-call when a function call is next.
+    ;; debug is for debug-on-entry function called.
+    (pcase (car debugger-args)
+      ((or `lambda `debug)
+       (insert "--entering a function:\n"))
+      ;; Exiting a function.
+      (`exit
+       (insert "--returning value: ")
+       (setq debugger-value (nth 1 debugger-args))
+       (prin1 debugger-value (current-buffer))
+       (insert ?\n)
+       (delete-char 1)
+       (insert ? )
+       (beginning-of-line))
+      ;; Debugger entered for an error.
+      (`error
+       (insert "--Lisp error: ")
+       (prin1 (nth 1 debugger-args) (current-buffer))
+       (insert ?\n))
+      ;; debug-on-call, when the next thing is an eval.
+      (`t
+       (insert "--beginning evaluation of function call form:\n"))
+      ;; User calls debug directly.
+      (_
+       (insert ": ")
+       (prin1 (if (eq (car debugger-args) 'nil)
+                  (cdr debugger-args) debugger-args)
+              (current-buffer))
+       (insert ?\n)))
+    ;; After any frame that uses eval-buffer,
+    ;; insert a line that states the buffer position it's reading at.
+    (save-excursion
+      (let ((tem eval-buffer-list))
+        (while (and tem
+                    (re-search-forward "^  eval-\\(buffer\\|region\\)(" nil t))
+          (beginning-of-line)
+          (insert (format "Error at line %d in %s: "
+                          (with-current-buffer (car tem)
+                            (line-number-at-pos (point)))
+                          (with-current-buffer (car tem)
+                            (buffer-name))))
+          (pop tem))))
+    (debugger-make-xrefs)))
+
 (setq csv-separators '(";" "	" ",")
       csv-separator-chars '(?\; ?	 ?,)
       csv--skip-regexp "^
