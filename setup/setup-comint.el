cdb187eb2935d2dadbad7e04669b50621d2b4e9b
Vasilij Schneidermann <v.schneidermann@gmail.com>
2014-09-21 23:41:31 +0200

    Finish port of CIDER REPL functionality

--- setup/setup-comint.el
+++ setup/setup-comint.el
@@ -16,24 +16,42 @@
   (propertize text 'read-only t))
 (add-hook 'comint-preoutput-filter-functions 'my-comint-preoutput-turn-buffer-read-only)
 
-(defun my-comint-clear-buffer ()
-  (interactive)
-  (let ((comint-buffer-maximum-size 0))
-    (comint-truncate-buffer)))
-
-;; TODO use this to implement CIDER-style buffer clearing
-;; see https://github.com/clojure-emacs/cider/blob/cb3509eb54d3c3369681d73f3218a1493b977e99/cider-repl.el#L640-L655
-(defun my-comint-goto-last-prompt ()
-  (interactive)
+(defun my-comint-goto-last-output-beg ()
   (comint-goto-process-mark)
-  (goto-char (previous-char-property-change (point)))
-  (while (not (looking-back comint-prompt-regexp))
-    (goto-char (previous-char-property-change (point)))))
+  (while (not (eq (get-char-property (point) 'field) 'boundary))
+    (goto-char (previous-char-property-change (point) (point-min))))
+  (goto-char (1+ (point))))
+
+(defun my-comint-goto-last-output-end ()
+  (comint-goto-process-mark)
+  (while (not (eq (get-char-property (point) 'font-lock-face)
+                  'comint-highlight-prompt))
+    (goto-char (previous-char-property-change (point) (point-min))))
+  (goto-char (1- (point))))
+
+;; stolen from https://github.com/clojure-emacs/cider/blob/cb3509eb54d3c3369681d73f3218a1493b977e99/cider-repl.el#L640-L655
+(defun my-comint-clear-last-output ()
+  (interactive)
+  (let ((start (save-excursion (my-comint-goto-last-output-beg)))
+        (end (save-excursion (my-comint-goto-last-output-end))))
+    (let ((inhibit-read-only t))
+      (delete-region start end)
+      (save-excursion
+        (goto-char start)
+        (insert (propertize "output cleared"
+                            'font-lock-face 'font-lock-comment-face))))))
+
+(defun my-recenter-top-bottom ()
+  (interactive)
+  (goto-char (point-max))
+  (let ((recenter-positions '(top bottom)))
+    (recenter-top-bottom)))
 
 (my-define-keys comint-mode-map
                   [remap kill-word] 'my-kill-word
                   [remap backward-kill-word] 'my-backward-kill-word
-                  (kbd "C-L") 'my-comint-clear-buffer)
+                  (kbd "C-S-l") 'my-comint-clear-last-output
+                  (kbd "C-l") 'my-recenter-top-bottom)
 
 (defun my-shell-turn-echo-off ()
   (setq comint-process-echoes t))
