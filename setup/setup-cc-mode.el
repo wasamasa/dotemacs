9a17e80e3a20a233c2f8e366aee736c74bac6948
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-01-27 19:37:36 +0100

    Deal with cc-mode

--- setup/setup-cc-mode.el
+++
@@ -1,6 +0,0 @@
-(my-eval-after cc-vars
-  (setq c-default-style '((java-mode . "java")
-                          (awk-mode . "awk")
-                          (other . "user"))))
-
-(provide 'setup-cc-mode)
