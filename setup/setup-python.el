920e32e1e19848675eb26c962cecc16e39230548
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-02-12 23:26:01 +0100

    Deal with Python and Ruby

--- setup/setup-python.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'interpreter-mode-alist '("python2" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python3" . python-mode))
-
-(add-hook 'python-mode-hook 'company-jedi-start)
-
-(provide 'setup-python)
--- setup/setup-ruby.el
+++
@@ -1,9 +0,0 @@
-(add-to-list 'auto-mode-alist '("Gemfile\\'" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile\\'" . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . enh-ruby-mode))
-
-(setq enh-ruby-bounce-deep-indent t)
-
-(setq inf-ruby-default-implementation "pry")
-
-(provide 'setup-ruby)
